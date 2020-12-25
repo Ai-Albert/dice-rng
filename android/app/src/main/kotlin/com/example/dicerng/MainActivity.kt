a0083342f355b6c626d5a4b04473cd06102fe180
Albert Ai <gsbalbert2002@gmail.com>
2020-12-25 11:36:13 -0800

    This is the first commit

---
+++ android/app/src/main/kotlin/com/example/dicerng/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.dicerng
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+void main() => runApp(MaterialApp(
+  title: 'Dice RNG',
+  home: Dice(),
+));
+
+class Dice extends StatefulWidget {
+  @override
+  _DiceState createState() => _DiceState();
+}
+
+class _DiceState extends State<Dice> {
+  int left = Random().nextInt(6) + 1;
+  int right = Random().nextInt(6) + 1;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.greenAccent,
+      appBar: AppBar(
+        backgroundColor: Colors.greenAccent,
+        centerTitle: true,
+        title: Text(
+          'Dice Roller',
+          style: TextStyle(
+            color: Colors.white,
+          ),
+        ),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.toll),
+            onPressed: () {
+              setState(() {
+                left = Random().nextInt(6) + 1;
+                right = Random().nextInt(6) + 1;
+              });
+            },
+          ),
+        ],
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: FlatButton(
+                child: Image.asset('assets/dice$left.png'),
+                onPressed: () {
+                  setState(() {
+                    left = Random().nextInt(6) + 1;
+                  });
+                },
+              ),
+            ),
+          ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: FlatButton(
+                child: Image.asset('assets/dice$right.png'),
+                onPressed: () {
+                  setState(() {
+                    right = Random().nextInt(6) + 1;
+                  });
+                },
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
